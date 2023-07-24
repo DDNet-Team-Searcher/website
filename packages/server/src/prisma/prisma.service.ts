dbe9406e265b1d4621e9cb67fc5e90f06ed29121
MilkeeyCat <40633857+MilkeeyCat@users.noreply.github.com>
2023-07-24 09:56:31 +0300

    fix: i always knew that i dont need that method

--- packages/server/src/prisma/prisma.service.ts
+++ packages/server/src/prisma/prisma.service.ts
@@ -1,14 +1,8 @@
-import { INestApplication, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    async enableShutdownHooks(app: INestApplication) {
-        this.$on('beforeExit', async () => {
-            await app.close();
-        });
-    }
-
     async exists<T extends { count: any }>(
         model: T,
         args: Parameters<T['count']>[0],
