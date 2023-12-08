a4a02336900e78ff1bcda602133a730c556dd6f6
MilkeeyCat <40633857+MilkeeyCat@users.noreply.github.com>
2023-12-08 19:11:30 +0200

    refactor: moved some stuff around

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
+
+# I use gentoo btw, I use tmux btw
 if [[ $(rc-service docker status) == *"stopped"* ]]; then
     sudo rc-service docker start
-    docker compose -f docker-compose.dev.yml up -d
+    docker compose -f db.yml up -d
 fi
 
 tmux neww $SHELL -c "pnpm run dev"
