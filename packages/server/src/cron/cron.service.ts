ff8cb302558e9471e0cc7a4eb9edaacb3765bc93
MilkeeyCat <40633857+MilkeeyCat@users.noreply.github.com>
2024-01-19 21:29:45 +0200

    fix(server): wasn't handling possible error when call startHappening

--- packages/server/src/cron/cron.service.ts
+++ packages/server/src/cron/cron.service.ts
@@ -10,7 +10,7 @@ export class CronService implements OnModuleInit {
     constructor(
         private readonly happeningsService: HappeningsService,
         private readonly notificationsService: NotificationsService,
-    ) {}
+    ) { }
 
     async onModuleInit() {
         const upcomingHappenings =
@@ -28,45 +28,49 @@ export class CronService implements OnModuleInit {
     async check(): Promise<void> {
         for (const happening of this.happenings) {
             if (happening && new Date() > happening.startAt) {
-                const success = await this.happeningsService.startHappening(
-                    happening.id,
-                );
+                try {
+                    const success = await this.happeningsService.startHappening(
+                        happening.id,
+                    );
 
-                if (success) {
-                    this.happenings.shift();
+                    if (success) {
+                        this.happenings.shift();
 
-                    const upcomingHappening =
-                        await this.happeningsService.nthUpcomingHappenings(10);
-                    if (upcomingHappening)
-                        this.happenings.push(upcomingHappening);
+                        const upcomingHappening =
+                            await this.happeningsService.nthUpcomingHappenings(10);
+                        if (upcomingHappening)
+                            this.happenings.push(upcomingHappening);
 
-                    await this.check();
-                } else {
-                    const playersInTeam = (
-                        await this.happeningsService.getHappeningInterestedPlayers(
-                            happening.id,
-                        )
-                    )[0];
-
-                    if (happening.status == Status.NotStarted) {
-                        for (const user of playersInTeam.interestedPlayers) {
-                            if (user.inTeam) {
-                                await this.notificationsService.sendNotification(
-                                    user.user.id,
-                                    NotificationType.NoEmptyServers,
-                                    {},
-                                );
-                            }
-                        }
-
-                        await this.happeningsService.updateStatus(
-                            happening.id,
-                            Status.InQueue,
-                        );
-
-                        happening.status = Status.InQueue;
                         await this.check();
+                    } else {
+                        const playersInTeam = (
+                            await this.happeningsService.getHappeningInterestedPlayers(
+                                happening.id,
+                            )
+                        )[0];
+
+                        if (happening.status == Status.NotStarted) {
+                            for (const user of playersInTeam.interestedPlayers) {
+                                if (user.inTeam) {
+                                    await this.notificationsService.sendNotification(
+                                        user.user.id,
+                                        NotificationType.NoEmptyServers,
+                                        {},
+                                    );
+                                }
+                            }
+
+                            await this.happeningsService.updateStatus(
+                                happening.id,
+                                Status.InQueue,
+                            );
+
+                            happening.status = Status.InQueue;
+                            await this.check();
+                        }
                     }
+                } catch (e) {
+                    // ¯\_(ツ)_/¯
                 }
             }
         }
