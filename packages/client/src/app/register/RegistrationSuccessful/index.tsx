929030c6d24b6da156ac9c48e839bf9c55b416fe
MilkeeyCat <milkeeycat@gmail.com>
2024-03-22 15:57:20 +0200

    refactor(client): remove unused `Stepper` component

--- packages/client/src/app/register/RegistrationSuccessful/index.tsx
+++ packages/client/src/app/register/RegistrationSuccessful/index.tsx
@@ -1,4 +1,3 @@
-import { Step } from '@/components/Stepper/Step';
 import { Button } from '@/components/ui/Button';
 import { useRouter } from 'next/navigation';
 
@@ -14,25 +13,23 @@ export function RegistrationSuccessful({ email }: OwnProps) {
     };
 
     return (
-        <Step>
-            <div className="text-high-emphasis p-10 bg-[#24201A] rounded-[20px] mt-[200px]">
-                <div className="flex items-center">
-                    <div>
-                        <p className="text-3xl font-medium">
-                            Your account has been successfully created.
-                        </p>
-                        <p>
-                            We sent an email to {email}. You have to open the
-                            link from email to activate your account. Only then
-                            you will be able to log in.
-                        </p>
-                    </div>
-                    <img src="/successful-register.png" />
+        <div className="text-high-emphasis p-10 bg-[#24201A] rounded-[20px] mt-[200px]">
+            <div className="flex items-center">
+                <div>
+                    <p className="text-3xl font-medium">
+                        Your account has been successfully created.
+                    </p>
+                    <p>
+                        We sent an email to {email}. You have to open the
+                        link from email to activate your account. Only then
+                        you will be able to log in.
+                    </p>
                 </div>
-                <Button styleType={'filled'} onClick={redirectToLoginPage}>
-                    Go to login page
-                </Button>
+                <img src="/successful-register.png" />
             </div>
-        </Step>
+            <Button styleType={'filled'} onClick={redirectToLoginPage}>
+                Go to login page
+            </Button>
+        </div>
     );
 }
--- packages/client/src/components/Stepper/Step.tsx
+++
@@ -1,7 +0,0 @@
-type OwnProps = {
-    children: React.ReactNode;
-};
-
-export function Step({ children }: OwnProps) {
-    return <div className="min-w-full">{children}</div>;
-}
