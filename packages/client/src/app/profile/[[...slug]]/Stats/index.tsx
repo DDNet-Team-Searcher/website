edb835d731384e6a09d3ed1800904b1a0dacdfd5
MilkeeyCat <milkeeycat@gmail.com>
2024-03-22 16:02:23 +0200

    refactor(client): move `Graph` component

--- packages/client/src/app/profile/[[...slug]]/Stats/index.tsx
+++ packages/client/src/app/profile/[[...slug]]/Stats/index.tsx
@@ -1,13 +1,79 @@
-import { Graph } from '@/components/Graph';
+import { useEffect, useState } from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend,
+    ChartData,
+    ChartOptions,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { useAppDispatch } from '@/utils/hooks/hooks';
+import { getUserStats } from '@/store/slices/user';
 
 type OwnProps = {
     username: string;
 };
 
 export function Stats({ username }: OwnProps) {
+    const [fetchedData, setFecthedData] = useState<
+        null | [string, number, number][]
+    >();
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        ChartJS.register(
+            CategoryScale,
+            LinearScale,
+            PointElement,
+            LineElement,
+            Title,
+            Tooltip,
+            Filler,
+            Legend,
+        );
+    }, []);
+
+    useEffect(() => {
+        dispatch(getUserStats(username)).then((res) => {
+            setFecthedData(res);
+        });
+    }, [username]);
+
+    const options: ChartOptions<'line'> = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top' as const,
+            },
+            title: {
+                display: true,
+                text: 'Points per year',
+            },
+        },
+    };
+
+    const data: ChartData<'line'> = {
+        labels: fetchedData?.map((row) => row[0]),
+        datasets: [
+            {
+                fill: true,
+                label: `${username}'s points`,
+                data: fetchedData?.map((row) => row[2])!,
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+        ],
+    };
+
     return (
         <section>
-            <Graph username={username} />
+            {fetchedData && <Line options={options} data={data} />}
         </section>
     );
 }
--- packages/client/src/app/register/RegistrationSuccessful/index.tsx
+++ packages/client/src/app/register/RegistrationSuccessful/index.tsx
@@ -20,9 +20,9 @@ export function RegistrationSuccessful({ email }: OwnProps) {
                         Your account has been successfully created.
                     </p>
                     <p>
-                        We sent an email to {email}. You have to open the
-                        link from email to activate your account. Only then
-                        you will be able to log in.
+                        We sent an email to {email}. You have to open the link
+                        from email to activate your account. Only then you will
+                        be able to log in.
                     </p>
                 </div>
                 <img src="/successful-register.png" />
