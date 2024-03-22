debcdfafbb4fb9eb62bd9227c739648fc6b1235e
MilkeeyCat <milkeeycat@gmail.com>
2024-03-22 11:49:45 +0200

    refactor(client): add types to `Line` component props

--- packages/client/src/components/Graph/index.tsx
+++ packages/client/src/components/Graph/index.tsx
@@ -9,6 +9,8 @@ import {
     Tooltip,
     Filler,
     Legend,
+    ChartData,
+    ChartOptions,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import { useAppDispatch } from '@/utils/hooks/hooks';
@@ -43,7 +45,7 @@ export function Graph({ username }: OwnProps) {
         });
     }, [username]);
 
-    const options = {
+    const options: ChartOptions<'line'> = {
         responsive: true,
         plugins: {
             legend: {
@@ -56,13 +58,13 @@ export function Graph({ username }: OwnProps) {
         },
     };
 
-    const data = {
+    const data: ChartData<'line'> = {
         labels: fetchedData?.map((row) => row[0]),
         datasets: [
             {
                 fill: true,
                 label: `${username}'s points`,
-                data: fetchedData?.map((row) => row[2]),
+                data: fetchedData?.map((row) => row[2])!,
                 borderColor: 'rgb(53, 162, 235)',
                 backgroundColor: 'rgba(53, 162, 235, 0.5)',
             },
